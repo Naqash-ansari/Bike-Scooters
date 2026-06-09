392a8c5fbe3f0430c3c2272851468d0139d43bbb
NaqashMuhammad <naqashansari55@gmail.com>
2026-06-09 07:36:17 +0100

    Use default Next build output

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
-
 /** @type {import('next').NextConfig} */
-const createConfig = (phase) => ({
-  distDir: phase === PHASE_DEVELOPMENT_SERVER ? ".next-dev" : ".next-build",
+const nextConfig = {
   images: {
     unoptimized: true
   }
-});
+};
 
-export default createConfig;
+export default nextConfig;
